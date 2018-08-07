96bccc589e2729c50403aa34d688bebba476d253
Doron Orenstein <jazziseverywhere@gmail.com>
2018-08-07 16:00:44 -0500

    got it working with router...I think

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import routes from './routes';
-// import registerServiceWorker from './registerServiceWorker';
+// import Routes from './routes';
+import ChainList from './components/ChainList';
+import ChainDetail from './components/ChainDetail';
+
+
+const routes = [
+  {
+    path: "/",
+    component: ChainList
+  },
+  {
+    path: "/chain-detail",
+    component: ChainDetail,
+  }
+];
+
+const Routes = route => (
+  <Route
+    path={route.path}
+    render={props => (
+      // pass the sub-routes down to keep nesting
+      <route.component {...props} routes={route.routes} />
+    )}
+  />
+);
 
 ReactDOM.render(
   <Router>
-    {routes}
+    <div>{routes.map((route, i) => <Routes key={i} {...route} />)}</div>
   </Router>,
   document.getElementById('root')
 );
--- src/routes.js
+++ src/routes.js
@@ -8,20 +8,31 @@ import { Route, Switch } from 'react-router-dom';
 // } from './components/App';
 
 import App from './components/App';
-import ChainList from './components/ChainList';
-import ChainDetail from './components/ChainDetail';
 
-// React router v4 - https://reacttraining.com/react-router/
-const RootRoutes = (
-  <Switch>
-    <Route exact path="/" component={ChainList}/>
-    <Route path="/chain-detail" component={ChainDetail}/>
-  </Switch>
-);
 
-export default routes (
+// then our route config
+const routes = [
+  {
+    path: "/",
+    component: ChainList
+  },
+  {
+    path: "/chain-detail",
+    component: ChainDetail,
+  }
+];
+
+
+// wrap <Route> and use this everywhere instead, then when
+// sub routes are added to any route it'll work
+const Routes = route => (
   <Route
-    path="/"
-    render={() => <App><RootRoutes /></App>}
+    path={route.path}
+    render={props => (
+      // pass the sub-routes down to keep nesting
+      <route.component {...props} routes={route.routes} />
+    )}
   />
 );
+
+export default Routes;
